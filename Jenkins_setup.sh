4683e4ddcbc92d0ac0b53535fa804315007bbc67
Devops-13999 <157839633+Devops-13999@users.noreply.github.com>
2024-06-01 11:26:17 +0530

    Update Jenkins_setup.sh

--- Jenkins_setup.sh
+++ Jenkins_setup.sh
@@ -6,6 +6,12 @@ sudo apt upgrade -y
 
 sudo apt install openjdk-17-jre -y
 
+#Install the GPG key
+#Curl - cmd tool to tarnfer data from and to server
+#sudo is for root user
+#| is to tranfer the output of first cmd as input to second cmd
+#tee Reads from standard input and writes to standard output and files.
+
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
